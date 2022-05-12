adab249f6af8a0cc4b2811f7fe6fd31819ab07b7
ricardo <luisricardogz@gmail.com>
2022-05-11 22:30:55 -0500

    agregando elementos para el pago

---
+++ app/src/main/java/com/example/firstapp/pagos/PagoAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.firstapp.pagos
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PagoAdapter (val listaPagos : List<PagoModel>) : RecyclerView.Adapter<PagoViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagoViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PagoViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/firstapp/pagos/PagoViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.firstapp.pagos
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PagoViewHolder (view : View) : RecyclerView.ViewHolder(view) {
+
+}
